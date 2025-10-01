c262a623b7add37deba4660e418b11f67b130e80
code-by-jalel <benromdhanejaleleddine@gmail.com>
2025-10-01 12:55:38 +0100

    update

--- tp3/hello.ts
+++ tp3/hello.ts
@@ -51,3 +51,89 @@ interface admin extends user{
     permissions: string[];
 }
 //exercice 5
+function add(a:number,b:number):number{
+    return a+b;
+}
+function greet(name:string,age ?:number):void{
+    if(age===undefined){
+        console.log(`bonjour ${name}`);
+    }else{
+        console.log(`bonjour ${name}, tu as ${age} ans`);
+    }
+}
+function power(base:number,exp:number=2){
+    let p=1;
+    for(var i =0;i<exp;i++){
+        p=p*base;
+    }
+    return p;
+}
+function combine(a:number|string,b:number|string):number|string{
+    if(typeof a =="number" && typeof b == "number"){
+        return a+b;
+    }
+    if(typeof a =="string" && typeof b == "string"){
+        return a+b;
+    }
+    throw new Error("Arguments must be both numbers or both strings");
+}
+//exercice 6
+//1.
+class person{
+    name:string;
+    age:number;
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+    public greet():void{
+        console.log(`greetings ${this.name},tu as ${this.age}`);
+    }   
+}
+//2.
+class student extends person{
+    school:string;
+    constructor(name:string,age:number,school:string){
+        super(name,age);
+        this.school=school;
+    }
+}
+//3.
+abstract class shape{
+    public abstract area():number;
+}
+class Rectangle extends shape {
+    width:number;
+    height:number;
+    public constructor(width: number, height: number) {
+        super();
+        this.width=width;
+        this.height=height;
+    }
+    public area(): number {
+        return this.width * this.height;
+    }
+}
+class circle extends shape{
+    radius:number;
+    public constructor(radius:number){
+        super();
+        this.radius=radius;
+    }
+    public area():number{
+        return Math.PI*this.radius*this.radius;
+    }
+}
+//4.
+interface Drivable {
+    drive(): void;
+}
+class Car implements Drivable {
+    brand: string;
+    constructor(brand: string) {
+        this.brand = brand;
+    }
+    drive(): void {
+        console.log(`${this.brand} is driving`);
+    }
+}
