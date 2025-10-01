5128abdd51a2ab92474fb612b17a833b7ec2f726
code-by-jalel <benromdhanejaleleddine@gmail.com>
2025-10-01 10:40:41 +0100

    update

--- tp3/hello.ts
+++ tp3/hello.ts
@@ -1,1 +1,15 @@
+//exercice 1
 console.log("Hello TypeScript!");
+
+//exercice 2
+
+let Name: string;
+let age: number;
+let isAdmin: boolean;
+const scores: number[] = [];
+let etudiant: [string,number];
+enum role{
+    user = "USER",
+    admin = "ADMIN",
+    SuperAdmin = "SUPERADMIN"
+};
